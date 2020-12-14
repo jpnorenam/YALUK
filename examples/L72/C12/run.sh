66fa2912192c15030d3d7c3f2f8065b9053c0f2b
ERNESTO PEREZ <eperezg@unal.edu.co>
2020-12-14 11:14:43 -0500

    Files updated for line L72, now lines are complete

---
+++ examples/L72/C12/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /vagrant/source
+make
+cd /vagrant/libatp
+chmod 755 vardimn
+chmod 755 vardim
+dos2unix vardimn
+./vardimn listsize.ylk
+make
+cp startup /vagrant/examples/L72/C12/
+cd /vagrant/examples/L72/C12/
+#gdb --args /vagrant/libatp/tpbig BOTH test_ESSA.atp s -r
+time /vagrant/libatp/tpbig BOTH L72_502_circuitC12.0.atp s -r
